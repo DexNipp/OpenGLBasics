e20317df4c2794475f878785674370a2511d56a2
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-05-07 21:56:14 -0500

    Add imgui & glm into the project

--- OpenGLBasics/src/Application.cpp
+++ OpenGLBasics/src/Application.cpp
@@ -1,5 +1,5 @@
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include "GLFW/include/GLFW/glfw3.h"
 
 #include <iostream>
 #include <fstream>
@@ -14,6 +14,12 @@
 #include "VertexArray.h"
 #include "Shader.h"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include "glm/glm.hpp"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -21,9 +27,9 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
+    
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -33,8 +39,7 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    glfwSwapInterval(1);
-
+     glfwSwapInterval(1);
 
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
@@ -78,28 +83,68 @@ int main(void)
         Renderer renderer;
 
         //////////////////////////////////////////////////////////////////////
+        bool show_demo_window = true;
+        bool show_another_window = false;
 
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
 
-        float rgb = 0.0f;
-        float increment = 0.01;
+        ImGuiIO& io = ImGui::GetIO();
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 410");
+
+        ImGui::StyleColorsDark();
+
+        float red = 0.3f, green = 0.3f, blue = 0.3f;
+       
+        glm::vec4 clear_color(0.45f, 0.55f, 0.60f, 1.00f);
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
             renderer.Clear();
 
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Test");                        
+
+                ImGui::Text("This is some useful text.");
+               
+                ImGui::SliderFloat("red", &red, 0.0f, 1.0f); 
+                
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+                red = clear_color.x;
+                green = clear_color.y;
+                blue = clear_color.z;
+
+                if (ImGui::Button("Button")) {
+                   
+                    counter++;
+                }
+                    
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::End();
+            }
+
             shader.Bind();
-            shader.SetUniform4f("u_Color", 1.0f, 0.2f, rgb, 1.0f);
+            shader.SetUniform4f("u_Color", red, green, blue, 1.0f);
 
             renderer.Draw(va, ib, shader);
 
-            if (rgb > 1.0f)
-                increment = -0.01f;
-            else if (rgb < 0.0f)
-                increment = 0.01f;
-
-            rgb += increment;
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
@@ -109,6 +154,10 @@ int main(void)
         }
     }
         
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     glfwTerminate();
     return 0;
 }
