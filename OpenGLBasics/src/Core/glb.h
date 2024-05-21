47328ab2677e2ed3f1d2c570818f20a4af8468b2
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-05-21 01:28:02 -0500

    Move frequently used includes into glb.h to clean up includes

---
+++ OpenGLBasics/src/Core/glb.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Core/Asserts.h"
+#include "Core/Timestep.h"
+#include "Core/Utility.h"
+
+#include "Renderer/Renderer.h"
+#include "Renderer/VertexBuffer.h"
+#include "Renderer/VertexBufferLayout.h"
+#include "Renderer/IndexBuffer.h"
+#include "Renderer/VertexArray.h"
+#include "Renderer/Shader.h"
+#include "Renderer/Shapes.h"
+
+#include "Core/Log.h"
