deaf43b5673072519a2a24294358c911cd990745
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-05-21 01:25:23 -0500

    Move Camera related files into renderer

---
+++ OpenGLBasics/src/Renderer/Camera.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <glm/gtc/quaternion.hpp>
+#include "glm/gtc/matrix_transform.hpp"
+
+struct Axis {
+    static constexpr glm::vec3 PositiveX{  1,  0,  0 };
+    static constexpr glm::vec3 NegativeX{ -1,  0,  0 };
+    static constexpr glm::vec3 PositiveY{  0,  1,  0 };
+    static constexpr glm::vec3 NegativeY{  0, -1,  0 };
+    static constexpr glm::vec3 PositiveZ{  0,  0,  1 };
+    static constexpr glm::vec3 NegativeZ{  0,  0, -1 };
+};
+
+// Default camera values
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+
+// TODO: Make Camera able to maintain itself in main loop. Add setters and getters for private vars and OnUpdate 
+
+class Camera {
+public:
+ 
+    float Fov;
+    float AspectRatio;
+    float zFar;
+    float zNear;
+
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Right;
+    glm::vec3 Up;
+    glm::vec3 WorldUp;
+
+    // euler Angles
+    float Yaw;
+    float Pitch;
+
+    Camera(float fov, float aspectRatio, float zFar, float zNear)
+        : Fov(fov), AspectRatio(aspectRatio), zFar(zFar), zNear(zNear), Position(0.0f),
+          Yaw(YAW), Pitch(PITCH), Front(Axis::NegativeZ), WorldUp(Axis::PositiveY)
+    {
+       
+        UpdateCameraVectors();
+    }
+
+    glm::mat4 getProjectionMatrix() const {
+        return glm::perspective(glm::radians(Fov), AspectRatio, zNear, zFar);
+    }
+
+    inline glm::mat4 getViewMatrix() const {
+        return glm::lookAt(Position, Position + Front, Up);
+    }
+
+    glm::mat4 getProjectionViewMatrix() const {
+        return getProjectionMatrix() * getViewMatrix();
+    }
+
+    void UpdateCameraVectors()
+    {
+        glm::vec3 front;
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        Front = glm::normalize(front);
+        // re-calculate the Right and Up vector
+        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        Up = glm::normalize(glm::cross(Right, Front));
+    }
+};
---
+++ OpenGLBasics/src/Renderer/CameraController.h
@@ -0,0 +1,89 @@
+#pragma once
+#include "Camera.h"
+#include <Core/KeyCodes.h>
+#include <Core/MouseCodes.h>
+
+namespace glb {
+
+
+    class CameraController {
+    public:
+        CameraController(Camera& camera, float movementSpeed = 10.0f, float mouseSensitivity = 0.1f)
+            : m_Camera(camera), m_MovementSpeed(movementSpeed), m_MouseSensitivity(mouseSensitivity)
+        {
+            m_LastX = 1920.0f / 2.0f;
+            m_LastY = 1080.0f / 2.0f;
+            m_FirstMouse = true;
+        }
+
+        void ProcessKeyboardInput(KeyCode keyCode, float deltaTime) {
+
+            float velocity = (keyCode == Key::LeftShift) ? m_MovementSpeed * deltaTime : (m_MovementSpeed * 0.5f)* deltaTime;
+            
+            if (keyCode == Key::W)
+                m_Camera.Position += m_Camera.Front * velocity;
+            if (keyCode == Key::S)
+                m_Camera.Position -= m_Camera.Front * velocity;
+            if (keyCode == Key::A)
+                m_Camera.Position -= m_Camera.Right * velocity;
+            if (keyCode == Key::D)
+                m_Camera.Position += m_Camera.Right * velocity;
+            if (keyCode == Key::Space)
+                m_Camera.Position += m_Camera.Up * velocity;
+            if (keyCode == Key::LeftAlt)
+                m_Camera.Position -= m_Camera.Up * velocity;
+        }
+
+        void ProcessMouseMovement(double win_xpos, double win_ypos) {
+            
+            float xpos = static_cast<float>(win_xpos);
+            float ypos = static_cast<float>(win_ypos);
+            if (m_FirstMouse)
+            {
+                m_LastX = xpos;
+                m_LastY = ypos;
+                m_FirstMouse = false;
+            }
+
+            float xoffset = xpos - m_LastX;
+            float yoffset = m_LastY - ypos; // reversed since y-coordinates go from bottom to top
+
+            m_LastX = xpos;
+            m_LastY = ypos;
+
+            xoffset *= m_MouseSensitivity;
+            yoffset *= m_MouseSensitivity;
+
+            m_Camera.Yaw += xoffset;
+            m_Camera.Pitch += yoffset;
+
+            // make sure that when pitch is out of bounds, screen doesn't get flipped
+
+            if (m_Camera.Pitch > 89.0f)
+                m_Camera.Pitch = 89.0f;
+            else if (m_Camera.Pitch < -89.0f)
+                m_Camera.Pitch = -89.0f;
+
+
+            // update Front, Right and Up Vectors using the updated Euler angles
+            m_Camera.UpdateCameraVectors();
+        }
+
+        void ProcessMouseScroll(float yoffset) {
+
+            m_Camera.Fov -= yoffset;
+            if (m_Camera.Fov < 1.0f)
+                m_Camera.Fov = 1.0f;
+            if (m_Camera.Fov > 120.0f)
+                m_Camera.Fov = 120.0f;
+        }
+
+    private:
+        Camera& m_Camera;
+        float m_MovementSpeed;
+        float m_MouseSensitivity;
+        float m_FirstMouse;
+        float m_LastX;
+        float m_LastY;
+    };
+}
